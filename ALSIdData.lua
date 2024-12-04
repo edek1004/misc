37ef87182ec3e2c884ab1a5860f4e2f3513c34cb
edek1004 <tongmo.jinedek@gmail.com>
2024-12-04 15:39:00 +0700

    Update ALSIdData.lua

--- ALSIdData.lua
+++ ALSIdData.lua
@@ -1,8 +1,8 @@
 return {
-  ["JirenEvo"] = "<:ALSJirenEvo:1313784245574959104> ",
-  ["GokuMUI"] = "<:ALSGokuMUI:1313784524618072085> ",
-  ["Goku_BlackEvo"] = "<:ALSGokuBlackEvo:1313784944560177173> ",
-  ["AiHoshinoEvo"] = "<:ALSAiHoshinoEvo:1313785199712141373> ",
-  ["BrolyEvo"] = "<:ALSBrolyEvo:1313785439005708308> ",
-  ["SukunaGodly"] = "<:ALSSukunaGodly:1313785639833047090> "
+  ["JirenEvo"] = "1313784245574959104",
+  ["GokuMUI"] = "1313784524618072085",
+  ["Goku_BlackEvo"] = "1313784944560177173",
+  ["AiHoshinoEvo"] = "1313785199712141373",
+  ["BrolyEvo"] = "1313785439005708308",
+  ["SukunaGodly"] = "1313785639833047090"
 }
