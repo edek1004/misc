7f450cc7e3357f2f117cf0f9d2a32345c73a9f4f
edek1004 <tongmo.jinedek@gmail.com>
2024-12-16 14:47:16 +0700

    Update ALSUnitImg.lua

--- ALSUnitImg.lua
+++ ALSUnitImg.lua
@@ -362,5 +362,6 @@ return {
   ["Archer"] = "<:ALSArcher:1316159699674075157> ",
   ["ArcherEvo"] = "<:ALSArcherEvo:1316159702945763368> ",
   ["Lancer"] = "<:ALSLancer:1316917519369637888> ",
-  ["LancerShiny"] = "<:ALSLancerShiny:1316917522863489054> "
+  ["LancerShiny"] = "<:ALSLancerShiny:1316917522863489054> ",
+  ["Ainz"] = "<:ALSAinz:1318122129648259125> "
 }
