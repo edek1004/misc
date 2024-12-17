e4dcca0253a6dfccc9d6660a6e4bd1c6387e968d
edek1004 <tongmo.jinedek@gmail.com>
2024-12-17 07:46:35 +0700

    Create AVModifierHandler.lua

---
+++ AVModifierHandler.lua
@@ -0,0 +1,4 @@
+return {
+  "Champions","Dodge","Drowsy","Quake","Immunity","Exploding","Shielded","Thrice","Regen","Revitalize",
+  "Slayer","Damage","Fast","Fisticuffs","Harvest","Precise Attack","King's Burden","Money Surge","No Trait No Problem","Press It","Cooldown","Strong","Warding off Evil","Planning Ahead","Common_Card","Uncommon_Card","Super Rare_Card","Rare_Card","Uncommon Loot","Common Loot","Range"
+}
